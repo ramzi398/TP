e37e6653c2fe06c96f91e0fd8a763055d1598300
Ramzi Benali <ramzi.benali@univ-tours.fr>
2023-10-14 16:04:47 +0200

    ajout classe

---
+++ Moyenne.java
@@ -0,0 +1,17 @@
+public class Moyenne {
+    int note1;
+    int note2;
+	Moyenne(int note1, int note2){
+		this.note1 = note1;
+        this.note2 = note2;
+	}
+	public int getNote1() {
+		return this.note1;
+	}
+    public int getNote2() {
+		return this.note2;
+	}
+  
+
+   
+}
