f6f4e909620e6627e1c243e302ce3d3d23a63d79
Ramzi Benali <ramzi.benali@univ-tours.fr>
2023-10-14 14:08:59 +0200

    my commit1

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,6 +1,6 @@
 public class SimpleTesting {
 	public static void main(String[] args) {
-		Student student = new Student("John12");
+		Student student = new Student("ramzi1");
 		String name = student.getName();
 		System.out.println("my name is : "+name);
 	}
