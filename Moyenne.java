68d03d6f981fb3b24ec7cd21ba9ccbb5d18009c1
Ramzi Benali <ramzi.benali@univ-tours.fr>
2023-10-14 16:13:37 +0200

    my commit6

---
+++ Moyenne.java
@@ -0,0 +1,17 @@
+public class Moyenne {
+    int noteA;
+    int noteB;
+	Moyenne(int noteA, int noteB){
+		this.noteA = noteA;
+        this.noteB = noteB;
+	}
+	public int getNote1() {
+		return this.noteA;
+	}
+    public int getNote2() {
+		return this.noteB;
+	}
+  
+
+   
+}
