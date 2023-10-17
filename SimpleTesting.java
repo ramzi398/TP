77f5ad0702f4138a3c7fcab37f1b0ccbe06018cc
Ramzi Benali <ramzi.benali@univ-tours.fr>
2023-10-17 18:35:29 +0200

    tt

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,7 +1,7 @@
 public class SimpleTesting {
 	public static void main(String[] args) {
 		Student student = new Student("Paul1");
-		Moyenne moy= new Moyenne(12, 10);
+		Moyenne moy= new Moyenne(13, 10);
 		String name = student.getName();
 		int nt1= moy.getNote1();
 		int nt2 = moy.getNote2();
